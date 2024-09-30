74f5a586789a5ea215b52833213abf6c4a89ef5f
darshan45672 <drshnbhandary@gmail.com>
2024-09-30 07:53:50 +0530

    fea: notify user on creation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\UserNotification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -75,6 +77,11 @@ class User extends Authenticatable
                 Storage::disk('public')->delete($user->image);
             }
         });
+    }
+
+    protected function afterCreate(): void
+    {
+        $this->record->notify(new UserNotification($this->record));
     }
     
 }
