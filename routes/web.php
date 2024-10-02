34f928562dcb770c3206dc04f08d767121b89462
Jnanesh <jtuluve@gmail.com>
2024-10-02 16:37:12 +0530

    Added event page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/events', function () {
+    return view('pages.events');
+})->name('events');
