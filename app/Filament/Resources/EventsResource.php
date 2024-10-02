894fc6c2a72887dde4c396dda880edf7ed660598
darshan45672 <drshnbhandary@gmail.com>
2024-10-02 21:11:38 +0530

    event resource crud done

---
+++ app/Filament/Resources/EventsResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EventsResource\Pages;
+use App\Filament\Resources\EventsResource\RelationManagers;
+use App\Models\Events;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EventsResource extends Resource
+{
+    protected static ?string $model = Events::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()->label('Event Name')->placeholder('Enter the event name'),
+                Select::make('event_type')->options([
+                    'workshop' => 'Workshop',
+                    'webinar' => 'Webinar',
+                    'seminar' => 'Seminar',
+                    'conference' => 'Conference',
+                    'expo' => 'Expo',
+                    'meetup' => 'Meetup',
+                    'hackathon' => 'Hackathon',
+                ])->required()->default('workshop'),
+                RichEditor::make('description')->columnSpanFull()->required()->label('Event Description')->placeholder('Enter the event description'),
+                FileUpload::make('banner')->required()->label('Event Banner'),
+                TextInput::make('location')->required()->label('Event Location')->placeholder('Enter the event location'),
+                TextInput::make('speaker')->required()->label('Speaker Name')->placeholder('Enter the speaker name'),
+                TextInput::make('speaker_mail')->required()->label('Speaker Email')->placeholder('Enter the speaker email'),
+                Select::make('status')->options([
+                    'draft' => 'Draft',
+                    'published' => 'Published',
+                    'cancelled' => 'Cancelled',
+                ])->required()->default('draft'),
+                TextInput::make('max_attendees')->required()->label('Max Attendees')->placeholder('Enter the maximum number of attendees'),
+                ToggleButtons::make('is_featured')->label('is Featured?')->boolean()->grouped()->default(false),
+                ToggleButtons::make('requires_registration')->label('Requires Registration?')->boolean()->grouped()->default(false),
+                ToggleButtons::make('has_certificate')->label('Has Certificate?')->boolean()->grouped()->default(false),
+                ToggleButtons::make('notify_attendees')->label('Notify Attendees?')->boolean()->grouped()->default(false),
+                ToggleButtons::make('notify_attendance')->label('Notify Attendance?')->boolean()->grouped()->default(false),
+                DatePicker::make('start_date')->required()->label('Start Date')->placeholder('Select the start date')->default(now()),
+                DatePicker::make('end_date')->required()->label('End Date')->placeholder('Select the end date')->default(now()),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                EditAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEvents::route('/'),
+            'create' => Pages\CreateEvents::route('/create'),
+            'edit' => Pages\EditEvents::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/EventsResource/Pages/CreateEvents.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\EventsResource\Pages;
+
+use App\Filament\Resources\EventsResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEvents extends CreateRecord
+{
+    protected static string $resource = EventsResource::class;
+}
