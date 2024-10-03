ac805672fb3af53f5613bcd8531e3f673a74e9af
HimanshuHegde <himanshuhegde797@gmail.com>
2024-10-03 23:56:00 +0530

    used components and integrated the new navbar design

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,183 @@
+<div class="navbar navbar-expand-lg fancy navbar-light navbar-bg-light caret-none">
+    <div class="container">
+        <nav
+            class="navbar-collapse-wrapper bg-white d-flex flex-row flex-nowrap w-100 justify-content-between align-items-center">
+            <div class="navbar-brand w-100">
+                <a href="index.html">
+                    <img src="{{ asset('assets/img/logo-dark.png') }}" class="img-fluid h-8 w-auto"
+                        srcset="./assets/img/logo-dark@2x.png 2x" alt="DevNation Logo" />
+                </a>
+            </div>
+            <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
+                <div class="offcanvas-header d-lg-none">
+                    <h3 class="text-white fs-30 mb-0">DevNation</h3>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                        aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body ms-lg-auto d-flex flex-column h-100">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown dropdown-mega">
+                            <a class="nav-link" href="{{ route('home') }}">Home</a>
+                            {{-- <ul class="dropdown-menu mega-menu mega-menu-dark mega-menu-img">
+                                            <li class="mega-menu-content mega-menu-scroll">
+                                                <ul
+                                                    class="row row-cols-1 row-cols-lg-6 gx-0 gx-lg-4 gy-lg-2 list-unstyled">
+                                                    <li class="col">
+                                                        <a class="dropdown-item" href="demo1.html">
+                                                            <figure class="rounded lift d-none d-lg-block"><img
+                                                                    src="assets/img/demos/mi1.jpg"
+                                                                    srcset="./assets/img/demos/mi1@2x.jpg 2x" alt="">
+                                                            </figure>
+                                                            <span class="d-lg-none">Demo 1</span>
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                                <!--/.row -->
+                                                <span class="d-none d-lg-flex"><i
+                                                        class="uil uil-direction"></i><strong>Scroll to view
+                                                        more</strong></span>
+                                            </li>
+                                            <!--/.mega-menu-content-->
+                                        </ul> --}}
+                            <!--/.dropdown-menu -->
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#">About Us</a>
+                            {{-- <ul class="dropdown-menu">
+                                            <li class="dropdown dropdown-submenu dropend"><a
+                                                    class="dropdown-item dropdown-toggle" href="#"
+                                                    data-bs-toggle="dropdown">Services</a>
+                                                <ul class="dropdown-menu">
+                                                    <li class="nav-item"><a class="dropdown-item"
+                                                            href="services.html">Services I</a></li>
+                                                    <li class="nav-item"><a class="dropdown-item"
+                                                            href="services2.html">Services II</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul> --}}
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="{{ route('events') }}">Events</a>
+                            {{-- <div class="dropdown-menu dropdown-lg">
+                                            <div class="dropdown-lg-content">
+                                                <div>
+                                                    <h6 class="dropdown-header">Project Pages</h6>
+                                                    <ul class="list-unstyled">
+                                                        <li><a class="dropdown-item" href="projects.html">Projects
+                                                                I</a></li>
+                                                        <li><a class="dropdown-item" href="projects2.html">Projects
+                                                                II</a></li>
+                                                        <li><a class="dropdown-item" href="projects3.html">Projects
+                                                                III</a></li>
+                                                        <li><a class="dropdown-item" href="projects4.html">Projects
+                                                                IV</a></li>
+                                                    </ul>
+                                                </div>
+                                                <!-- /.column -->
+                                                <div>
+                                                    <h6 class="dropdown-header">Single Projects</h6>
+                                                    <ul class="list-unstyled">
+                                                        <li><a class="dropdown-item" href="single-project.html">Single
+                                                                Project I</a></li>
+                                                        <li><a class="dropdown-item" href="single-project2.html">Single
+                                                                Project II</a></li>
+                                                        <li><a class="dropdown-item" href="single-project3.html">Single
+                                                                Project III</a></li>
+                                                        <li><a class="dropdown-item" href="single-project4.html">Single
+                                                                Project IV</a></li>
+                                                    </ul>
+                                                </div>
+                                                <!-- /.column -->
+                                            </div>
+                                            <!-- /auto-column -->
+                                        </div> --}}
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link " href="#">Gallery</a>
+                            {{-- data-bs-toggle="dropdown">Blog</a> --}}
+                            {{-- <ul class="dropdown-menu">
+                                            <li class="nav-item"><a class="dropdown-item" href="blog.html">Blog
+                                                    without Sidebar</a></li>
+                                            <li class="nav-item"><a class="dropdown-item" href="blog2.html">Blog with
+                                                    Sidebar</a></li>
+                                            <li class="nav-item"><a class="dropdown-item" href="blog3.html">Blog with
+                                                    Left Sidebar</a></li>
+                                            <li class="dropdown dropdown-submenu dropend"><a
+                                                    class="dropdown-item dropdown-toggle" href="#"
+                                                    data-bs-toggle="dropdown">Blog Posts</a>
+                                                <ul class="dropdown-menu">
+                                                    <li class="nav-item"><a class="dropdown-item"
+                                                            href="blog-post.html">Post without Sidebar</a></li>
+                                                    <li class="nav-item"><a class="dropdown-item"
+                                                            href="blog-post2.html">Post with Sidebar</a></li>
+                                                    <li class="nav-item"><a class="dropdown-item"
+                                                            href="blog-post3.html">Post with Left Sidebar</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul> --}}
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="{{ route('contact') }}">Contact Us</a>
+                            {{-- data-bs-toggle="dropdown">Blog</a> --}}
+                            {{-- <ul class="dropdown-menu">
+                                            <li class="nav-item"><a class="dropdown-item" href="blog.html">Blog
+                                                    without Sidebar</a></li>
+                                            <li class="nav-item"><a class="dropdown-item" href="blog2.html">Blog with
+                                                    Sidebar</a></li>
+                                            <li class="nav-item"><a class="dropdown-item" href="blog3.html">Blog with
+                                                    Left Sidebar</a></li>
+                                            <li class="dropdown dropdown-submenu dropend"><a
+                                                    class="dropdown-item dropdown-toggle" href="#"
+                                                    data-bs-toggle="dropdown">Blog Posts</a>
+                                                <ul class="dropdown-menu">
+                                                    <li class="nav-item"><a class="dropdown-item"
+                                                            href="blog-post.html">Post without Sidebar</a></li>
+                                                    <li class="nav-item"><a class="dropdown-item"
+                                                            href="blog-post2.html">Post with Sidebar</a></li>
+                                                    <li class="nav-item"><a class="dropdown-item"
+                                                            href="blog-post3.html">Post with Left Sidebar</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul> --}}
+                        </li>
+                    </ul>
+                    <!-- /.navbar-nav -->
+                    <div class="offcanvas-footer d-lg-none">
+                        <div>
+                            <a href="cdn-cgi/l/email-protection.html#d3b5baa1a0a7fdbfb2a0a793b6beb2babffdb0bcbe"
+                                class="link-inverse"><span class="__cf_email__"
+                                    data-cfemail="a6cfc8c0c9e6c3cbc7cfca88c5c9cb">[email&#160;protected]</span></a>
+                            <br /> 00 (123) 456 78 90 <br />
+                            <nav class="nav social social-white mt-4">
+                                <a href="#"><i class="uil uil-linkedin"></i></a>
+                                <a href="#"><i class="uil uil-youtube"></i></a>
+                                <a href="#"><i class="uil uil-instagram"></i></a>
+                            </nav>
+                            <!-- /.social -->
+                        </div>
+                    </div>
+                    <!-- /.offcanvas-footer -->
+                </div>
+                <!-- /.offcanvas-body -->
+            </div>
+            <!-- /.navbar-collapse -->
+            <div class="navbar-other w-100 d-flex ms-auto">
+                <ul class="navbar-nav flex-row align-items-center ms-auto">
+                    <li class="nav-item">
+                        <nav class="nav social social-muted justify-content-end text-end">
+                            <a href="#"><i class="uil uil-linkedin"></i></a>
+                            <a href="#"><i class="uil uil-youtube"></i></a>
+                            <a href="#"><i class="uil uil-instagram"></i></a>
+                        </nav>
+                        <!-- /.social -->
+                    </li>
+                    <li class="nav-item d-lg-none">
+                        <button class="hamburger offcanvas-nav-btn"><span></span></button>
+                    </li>
+                </ul>
+                <!-- /.navbar-nav -->
+            </div>
+            <!-- /.navbar-other -->
+        </nav>
+    </div>
+</div>
