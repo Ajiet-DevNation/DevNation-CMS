32c334b56e267c0bd5a247f65f254775b47af3c3
Jnanesh <jtuluve@gmail.com>
2024-10-04 10:38:00 +0530

    Added events seeder and dynamic events for the frontend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Branch;
 use App\Models\College;
 use App\Models\Role;
 use App\Models\User;
+use App\Models\Events;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -49,5 +50,36 @@ class DatabaseSeeder extends Seeder
             'phone' => '1234567890',
 
         ]);
+        for ($i=0; $i < 10; $i++) {
+            $randDate = randomDate(date('Y-m-d', strtotime('-10 days')), date('Y-m-d', strtotime('+10 days')));
+            Events::factory()->create([
+                'name' => 'Webinar',
+                'description' => 'Webinar on Laravel',
+                'event_type' => 'webinar',
+                'location' => 'Pune, India',
+                'banner' => '/assets/img/photos/b6.jpg',
+                'speaker' => 'John Doe',
+                'speaker_mail' => 'jdoe@me.com',
+                'status' => 'published',
+                'is_featured' => true,
+                'requires_registration' => true,
+                'max_attendees' => 100,
+                'start_date' => $randDate,
+                'end_date' => now(),
+            ]);
+        }
     }
+}
+
+function randomDate($sStartDate, $sEndDate, $sFormat = 'Y-m-d H:i:s')
+{
+    // Convert the supplied date to timestamp
+    $fMin = strtotime($sStartDate);
+    $fMax = strtotime($sEndDate);
+
+    // Generate a random number from the start and end dates
+    $fVal = mt_rand($fMin, $fMax);
+
+    // Convert back to the specified date format
+    return date($sFormat, $fVal);
 }
--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -4,175 +4,34 @@
         <section class="m-8">
             <h1>Upcoming Events</h1>
             <div class="cards-wrapper">
-
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i class="uil uil-map-marker fs-15"></i>AJIET,
-                                    Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                        <button><span>Register Now{{-- <i class="uil uil-arrow-up-right"></i> --}}</span></button>
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i class="uil uil-map-marker fs-15"></i>AJIET,
-                                    Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                        <button><span>Register Now{{-- <i class="uil uil-arrow-up-right"></i> --}}</span></button>
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i class="uil uil-map-marker fs-15"></i>AJIET,
-                                    Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                        <button><span>Register Now{{-- <i class="uil uil-arrow-up-right"></i> --}}</span></button>
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i class="uil uil-map-marker fs-15"></i>AJIET,
-                                    Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                        <button><span>Register Now{{-- <i class="uil uil-arrow-up-right"></i> --}}</span></button>
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i class="uil uil-map-marker fs-15"></i>AJIET,
-                                    Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                        <button><span>Register Now{{-- <i class="uil uil-arrow-up-right"></i> --}}</span></button>
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i
-                                        class="uil uil-map-marker fs-15"></i>AJIET, Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                        <button><span>Register Now{{-- <i class="uil uil-arrow-up-right"></i> --}}</span></button>
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
+                @foreach ($upcomingEvents as $event)
+                    <article>
+                        <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
+                                    src={{ $event->banner }} alt=""><span class="bg"></span></a>
+                            <figcaption>
+                                <h5 class="from-top mb-0">Read More</h5>
+                            </figcaption>
+                        </figure>
+                        <div class="post-header">
+                            <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
+                                    href="./blog-post.html">{{ $event->name }}</a></h2>
+                        </div>
+                        <!-- /.post-header -->
+                        <div class="post-footer">
+                            <ul class="post-meta text-center">
+                                <li class="post-date"><i
+                                        class="uil uil-calendar-alt"></i><span>{{ $event->start_date }}</span>
+                                </li>
+                                <li class="post-comments"><a href="#"><i
+                                            class="uil uil-map-marker fs-15"></i>{{ $event->location }}</a>
+                                </li>
+                            </ul>
+                            <!-- /.post-meta -->
+                            <button><span>Register Now{{-- <i class="uil uil-arrow-up-right"></i> --}}</span></button>
+                        </div>
+                        <!-- /.post-footer -->
+                    </article>
+                @endforeach
             </div>
         </section>
     </main>
@@ -180,115 +39,33 @@
         <section class="m-8">
             <h1>Past Events</h1>
             <div class="cards-wrapper">
-
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i
-                                        class="uil uil-map-marker fs-15"></i>AJIET, Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i
-                                        class="uil uil-map-marker fs-15"></i>AJIET, Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i
-                                        class="uil uil-map-marker fs-15"></i>AJIET, Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
-                <article>
-                    <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
-                                src="./assets/img/photos/b6.jpg" alt=""><span class="bg"></span></a>
-                        <figcaption>
-                            <h5 class="from-top mb-0">Read More</h5>
-                        </figcaption>
-                    </figure>
-                    <div class="post-header">
-                        <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
-                                href="./blog-post.html">Ultricies fusce
-                                porta</a></h2>
-                    </div>
-                    <!-- /.post-header -->
-                    <div class="post-footer">
-                        <ul class="post-meta text-center">
-                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2022</span>
-                            </li>
-                            <li class="post-comments"><a href="#"><i
-                                        class="uil uil-map-marker fs-15"></i>AJIET, Mangaluru</a>
-                            </li>
-                        </ul>
-                        <!-- /.post-meta -->
-                    </div>
-                    <!-- /.post-footer -->
-                </article>
-
-                <!-- /article -->
+                @foreach ($pastEvents as $event)
+                    <article>
+                        <figure class="overlay overlay-1 hover-scale rounded mb-6"><a href="#"> <img
+                                    src={{ $event->banner }} alt=""><span class="bg"></span></a>
+                            <figcaption>
+                                <h5 class="from-top mb-0">Read More</h5>
+                            </figcaption>
+                        </figure>
+                        <div class="post-header">
+                            <h2 class="post-title h3 mb-3 text-center"><a class="link-dark"
+                                    href="./blog-post.html">{{ $event->name }}</a></h2>
+                        </div>
+                        <!-- /.post-header -->
+                        <div class="post-footer">
+                            <ul class="post-meta text-center">
+                                <li class="post-date"><i
+                                        class="uil uil-calendar-alt"></i><span>{{ $event->start_date }}</span>
+                                </li>
+                                <li class="post-comments"><a href="#"><i
+                                            class="uil uil-map-marker fs-15"></i>{{ $event->location }}</a>
+                                </li>
+                            </ul>
+                            <!-- /.post-meta -->
+                        </div>
+                        <!-- /.post-footer -->
+                    </article>
+                @endforeach
             </div>
         </section>
     </main>
