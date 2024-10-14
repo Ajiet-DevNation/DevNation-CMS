37fa8e35f815b0a34a5c5dab2085d8e955638755
darshan45672 <drshnbhandary@gmail.com>
2024-10-14 21:31:05 +0530

    updated clound front

--- app/Models/SocialAccounts.php
+++ app/Models/SocialAccounts.php
@@ -10,8 +10,7 @@ class SocialAccounts extends Model
     /** @use HasFactory<\Database\Factories\SocialAccountsFactory> */
     use HasFactory;
 
-    protected $fillable = [ 'github', 'linkedIn', 'twitter', 'facebook', 'instagram', 'website', 'others', 'is_verified',
-    ];
+    protected $fillable = [ 'github', 'linkedIn', 'twitter', 'facebook', 'instagram', 'website', 'others', 'is_verified'];
 
     public function user()
     {
