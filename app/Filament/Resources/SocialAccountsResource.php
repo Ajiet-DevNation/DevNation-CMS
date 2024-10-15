51ed84e8face832494088ee3ee73027b4a2ebf55
darshan45672 <drshnbhandary@gmail.com>
2024-10-15 16:07:44 +0530

    fea: social acc resource

--- app/Filament/Resources/SocialAccountsResource.php
+++ app/Filament/Resources/SocialAccountsResource.php
@@ -5,7 +5,11 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\SocialAccountsResource\Pages;
 use App\Filament\Resources\SocialAccountsResource\RelationManagers;
 use App\Models\SocialAccounts;
+use Faker\Provider\ar_EG\Text;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -23,7 +27,18 @@ class SocialAccountsResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Select::make('user_id')->relationship('user', 'name')->required(),
+                TextInput::make('github')
+                ->required()
+                ->url()
+                ->rule('regex:/^https:\/\/github\.com\/[A-Za-z0-9]+$/'),            
+                TextInput::make('linkedIn')->required(),
+                TextInput::make('twitter')->required(),
+                TextInput::make('facebook')->required(),
+                TextInput::make('instagram')->required(),
+                TextInput::make('website')->required(),
+                TextInput::make('others')->required(),
+                ToggleButtons::make('is_verified')->label('is Verfied')->boolean()->grouped()->default(false),
             ]);
     }
 
