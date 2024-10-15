5355f6f3553f9e645755d2dfdbfb682c8ce023cf
darshan45672 <drshnbhandary@gmail.com>
2024-10-15 22:26:27 +0530

    upgrade: social account reosurce list

--- app/Filament/Resources/SocialAccountsResource.php
+++ app/Filament/Resources/SocialAccountsResource.php
@@ -13,6 +13,14 @@ use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -21,7 +29,7 @@ class SocialAccountsResource extends Resource
 {
     protected static ?string $model = SocialAccounts::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-device-phone-mobile';
 
     public static function form(Form $form): Form
     {
@@ -34,7 +42,7 @@ class SocialAccountsResource extends Resource
                 TextInput::make('facebook')->required(),
                 TextInput::make('instagram')->required(),
                 TextInput::make('website')->required(),
-                TextInput::make('others')->required(),
+                TextInput::make('others'),
                 ToggleButtons::make('is_verified')->label('is Verfied')->boolean()->grouped()->default(false),
             ]);
     }
@@ -43,17 +51,40 @@ class SocialAccountsResource extends Resource
     {
         return $table
             ->columns([
-                //
+                // TextColumn::make('user.name')->label('User'),
+                TextColumn::make('user.email')->label('User Email'),
+                TextColumn::make('github')->label('Github')->getStateUsing(fn($record) => basename(parse_url($record->github, PHP_URL_PATH))) ->url(fn($record) => $record->github)->openUrlInNewTab(),
+                TextColumn::make('linkedIn')->label('LinkedIn')->getStateUsing(fn($record) => basename(parse_url($record->linkedIn, PHP_URL_PATH)))
+                ->url(fn($record) => $record->linkedIn)
+                ->openUrlInNewTab(),
+                TextColumn::make('twitter')->label('Twitter')->getStateUsing(fn($record) => basename(parse_url($record->twitter, PHP_URL_PATH)))
+                ->url(fn($record) => $record->twitter)
+                ->openUrlInNewTab(),
+                TextColumn::make('facebook')->label('Facebook')->getStateUsing(fn($record) => basename(parse_url($record->facebook, PHP_URL_PATH)))
+                ->url(fn($record) => $record->facebook)
+                ->openUrlInNewTab()->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('instagram')->label('Instagram')->getStateUsing(fn($record) => basename(parse_url($record->instagram, PHP_URL_PATH)))
+                ->url(fn($record) => $record->instagram)
+                ->openUrlInNewTab(),
+                TextColumn::make('website')->label('Website')->getStateUsing(fn($record) => parse_url($record->website, PHP_URL_HOST))
+                ->url(fn($record) => $record->website)->toggleable(isToggledHiddenByDefault: true)
+                ->openUrlInNewTab(),
+                TextColumn::make('others')->label('Others'),
+                IconColumn::make('is_verified')->label('Is Verified')->boolean(),  
             ])
             ->filters([
-                //
+                //9880078166
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ]),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
                 ]),
             ]);
     }
