6a156c217253bbeded6cf6fba0a53d68397ec93b
sthuthidolpady <sthuthidolpady@gmail.com>
2024-10-18 22:22:57 +0530

    updated certificate template resource

--- app/Filament/Resources/CertificateTemplateResource.php
+++ app/Filament/Resources/CertificateTemplateResource.php
@@ -5,13 +5,18 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CertificateTemplateResource\Pages;
 use App\Filament\Resources\CertificateTemplateResource\RelationManagers;
 use App\Models\CertificateTemplate;
+use Faker\Core\File;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use SebastianBergmann\CodeUnit\FileUnit;
 
 class CertificateTemplateResource extends Resource
 {
@@ -23,7 +28,10 @@ class CertificateTemplateResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('template_name')->required(),
+                FileUpload::make('template_image'),
+                Textarea::make('description'),
+                
             ]);
     }
 
