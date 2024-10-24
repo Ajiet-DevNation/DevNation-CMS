48bac217e398386a05b763fcc78e3d35e937e37c
darshan45672 <drshnbhandary@gmail.com>
2024-10-24 08:09:15 +0530

    updated certificatetemplate model and resource

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -9,4 +9,28 @@ class Certificate extends Model
 {
     /** @use HasFactory<\Database\Factories\CertificateFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'certificate_template_id',
+        'issued_at',
+    ];
+
+
+    public function event()
+    {
+        return $this->belongsTo(Events::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function certificateTemplate()
+    {
+        return $this->belongsTo(CertificateTemplate::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ Route::get('/event-details/{id}', [HomeController::class, 'eventDetails'])->name
 Route::get('/about',[HomeController::class, 'about'])->name('about');
 Route::get('/team', [TeamController::class, 'team'])->name('team');
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticate'])->name('user.authenticate');
+Route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'authenticate'])->name('user.authenticate')->middleware('guest');
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
-Route::get('/register', [AuthController::class, 'showRegister'])->name('user.showRegister');
-Route::post('/register', [AuthController::class, 'register'])->name('user.register');
+Route::get('/register', [AuthController::class, 'register'])->name('user.showRegister')->middleware('guest');
+Route::post('/register', [AuthController::class, 'register'])->name('user.register')->middleware('guest');
 
 Route::get('/profile', [ProfileController::class, 'index' ])->name('profile.index')->middleware('auth');
 
