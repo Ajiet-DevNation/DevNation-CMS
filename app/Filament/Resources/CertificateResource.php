eb411bada778837b047463d81b64ec57f3dc22f2
darshan45672 <drshnbhandary@gmail.com>
2024-10-24 13:17:01 +0530

    defined create read and edit certificate resource

---
+++ app/Filament/Resources/CertificateResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CertificateResource\Pages;
+use App\Filament\Resources\CertificateResource\RelationManagers;
+use App\Models\Certificate;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CertificateResource extends Resource
+{
+    protected static ?string $model = Certificate::class;
+    protected static ?string $modelLabel = 'Certificate';
+    protected static ?string $navigationIcon = 'heroicon-o-document';
+    protected static ?string $navigationLabel = 'Certificates';
+    protected static ?string $navigationGroup = 'Certificate Settings';
+    protected static ?string $navigationBadgeTooltip = 'The number of templates';
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('user_id')->relationship('user', 'name')->required()->searchable()->preload(),
+                Select::make('event_id')->relationship('event', 'name')->required()->searchable()->preload(),
+                Select::make('certificate_template_id')->required()->relationship('certificateTemplate', 'template_name')->searchable()->preload(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                EditAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCertificates::route('/'),
+            'create' => Pages\CreateCertificate::route('/create'),
+            'edit' => Pages\EditCertificate::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/CertificateResource/Pages/ListCertificates.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\CertificateResource\Pages;
+
+use App\Filament\Resources\CertificateResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListCertificates extends ListRecords
+{
+    protected static string $resource = CertificateResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
