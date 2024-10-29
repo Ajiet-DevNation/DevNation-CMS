fd65ffd8f918a37e358ecaf6878f460be85e9d4d
Katarina Baskovska <katarina.baskovska@student.tuke.sk>
2024-10-29 19:09:36 +0100

    delete some testing commands

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
-use Intervention\Image\ImageManagerStatic as Image;
-use Illuminate\Support\Facades\Storage;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
@@ -19,22 +17,3 @@ Route::get('/profile', [ProfileController::class, 'index' ])->name('profile.inde
 
 Route::get('/gallery', [HomeController::class, 'gallery'])->name('gallery.index');
 Route::get('/gallery/{id}', [HomeController::class, 'galleryDetails'])->name('gallery.show');
-
-Route::get('/test-image', function () {
-    // Skontroluj, či súbor existuje, alebo použi konkrétnu cestu k obrázku
-    $imagePath = Storage::disk('public')->path('gallery_images/test.png');
-
-    if (!file_exists($imagePath)) {
-        return 'Image not found!';
-    }
-
-    // Skús zmenšiť a uložiť obrázok
-    $resizedImage = Image::make($imagePath)->resize(800, 800, function ($constraint) {
-        $constraint->aspectRatio();
-        $constraint->upsize();
-    })->encode();
-
-    Storage::disk('public')->put('gallery_images/test_resized.png', (string) $resizedImage);
-
-    return 'Image resized and saved as test_resized.png';
-});
