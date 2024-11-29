b9ea8a3d9ef46971f7bb7e7a891d2ce3d38ad8d1
LKyz24 <kyzekova.lenka@gmail.com>
2024-11-29 23:35:28 +0100

    conference name change in test

--- tests/Feature/EventsTest.php
+++ tests/Feature/EventsTest.php
@@ -16,7 +16,7 @@ class EventsTest extends TestCase
         // Vytvor event bez banner obrázka
         $event = Events::create([
             'name' => 'Test Event',
-            'event_type' => 'Conference',
+            'event_type' => 'conference',
             'description' => 'This is a test event',
             'start_date' => now(),
             'end_date' => now()->addDays(1),
@@ -44,7 +44,7 @@ class EventsTest extends TestCase
         // Vytvor event s banner obrázkom
         $event = Events::create([
             'name' => 'Test Event with Banner',
-            'event_type' => 'Conference',
+            'event_type' => 'conference',
             'description' => 'This is a test event with a banner',
             'start_date' => now(),
             'end_date' => now()->addDays(1),
@@ -76,7 +76,7 @@ class EventsTest extends TestCase
         // Vytvor event s banner obrázkom
         $event = Events::create([
             'name' => 'Test Event with Resized Banner',
-            'event_type' => 'Conference',
+            'event_type' => 'conference',
             'description' => 'This is a test event with a resized banner',
             'start_date' => now(),
             'end_date' => now()->addDays(1),
